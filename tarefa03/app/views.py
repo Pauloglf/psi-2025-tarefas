42c7c4128aa7607c9f8aa3b9be076f1d8c7db3ae
Paulo Gabriel <96998821+Pauloglf@users.noreply.github.com>
2025-06-15 09:19:11 -0300

    detalhe

--- tarefa03/app/views.py
+++ tarefa03/app/views.py
@@ -13,4 +13,4 @@ def index(request):
         {'Nome': 'Beltrano', 'Matrícula': '202311181110001', 'Idade': '18', 'Cidade': 'Pau dos ferros'},
         {'Nome': 'Véyr', 'Matrícula': '202311181110001', 'Idade': '17', 'Cidade': 'Elói de Souza'},
         ]
-    return render(request, "usuarios.html")
+    return render(request, "usuarios.html", dados_usuarios)
