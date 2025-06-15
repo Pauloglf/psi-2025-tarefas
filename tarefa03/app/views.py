b98240b1b62e6f807e1661fb2ce4b7e54735b028
Paulo Gabriel <96998821+Pauloglf@users.noreply.github.com>
2025-06-15 09:18:27 -0300

    tarefa03

---
+++ tarefa03/app/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, "index.html")
+
+
+def index(request):
+    dados_usuarios =[
+        {'Nome': 'Fulano', 'Matrícula': '202311181110001', 'Idade': '17', 'Cidade': 'São Paulo do Potengi'},
+        {'Nome': 'Cicrano', 'Matrícula': '202311181110001', 'Idade': '17', 'Cidade': 'Santa Maria'},
+        {'Nome': 'Bruce', 'Matrícula': '202311181110001', 'Idade': '18', 'Cidade': 'Gotham'},
+        {'Nome': 'Beltrano', 'Matrícula': '202311181110001', 'Idade': '18', 'Cidade': 'Pau dos ferros'},
+        {'Nome': 'Véyr', 'Matrícula': '202311181110001', 'Idade': '17', 'Cidade': 'Elói de Souza'},
+        ]
+    return render(request, "usuarios.html")
